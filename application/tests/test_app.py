b104bfb4f3f9bfaad6acab6301b83a49dbf54041
Ubuntu <ubuntu@ip-172-31-26-69.us-east-2.compute.internal>
2021-02-02 15:20:51 +0000

    updated tests

--- application/tests/test_app.py
+++ application/tests/test_app.py
@@ -8,60 +8,21 @@ from application.models import Owner, Business
 
 class TestBase(TestCase):
     def create_app(self):
-        app.config.update(SQLALCHEMY_DATABASE_URI="mysql+pymysql://bilalhalal:Maliha0919!@localhost/halalworlddb",
-                SECRET_KEY="TEST_KEY",
+        app.config.update(SQLALCHEMY_DATABASE_URI="mysql+pymysql://bilalhalal:password@localhost/halalworlddb",
+                SECRET_KEY='YOUR_SECRET_KEY',
                 DEBUG=TRUE
                 )
         return app
 
     def setUp(self):
         db.create_all()
-        sample1 = Owner(name = 'John', business = 'Doe')
+        sampleowner = Owner(name = 'John')
+        samplebusiness = Business(name = "JohnO Ltd.", owner = sampleowner.name)
         db.session.add(sample1)
+        db.session.add(samplebusiness)
         db.session.commit()
 
     def tearDown(self):
         db.session.remove()
         db.drop_all()
 
-class TestViews(TestBase):
-
-    def test_home_get(self):
-        response = self.client.get(url_for('home'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_add_get(self):
-        response = self.client.get(url_for('add'))
-        self.assertEqual(response.status_code, 405)
-
-    def test_update_get(self):
-        response = self.client.get(url_for('update'))
-        self.assertEqual(response.status_code,405)
-
-    def test_delete_get(self):
-        response = self.client.get(url_for('delete'))
-        self.assertEqual(response.status_code,405)
-
-class TestAdd(TestBase):
-    def test_add_post(self):
-        response = self.client.post(
-                url_for('/add'),
-                data = dict(name="MrMan", business="New Business")
-            )
-        self.assertIn(b'MrMan',response.data)
-
-class TestUpdate(TestBase):
-    def test_update_post(self):
-        response = self.client.post(
-                url_for('update'),
-                data = dict(newname="MrsLady"),
-                )
-        self.assertEqual(response.status_code,200)
-
-class TestDelete(TestBase):
-    def test_delete_post(self):
-        response = self.client.post(
-                url_for('delete'),
-                data = dict(newname="MrsLady")
-                )
-        self.assertEqual(response.status_code,200)
