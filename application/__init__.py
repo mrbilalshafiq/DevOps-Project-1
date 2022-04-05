5df8c8cf4e4d9ba00bb38078101a3985ba8778d8
Bilal Shafiq <75343545+mrbilalshafiq@users.noreply.github.com>
2022-04-05 23:36:22 +0100

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://bilalhalal:password@halalworlddb.cuctwwcywsp7.eu-west-2.rds.amazonaws.com/halalworlddb"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://bilalhalal:Maliha0919!@localhost/halalworlddb"
 app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
 
 db = SQLAlchemy(app)
